9162f131f4b8d1814705693d2d3aebef7fb6d869
jelena-vlajkov <vlajkovj31@gmail.com>
2021-06-18 02:02:42 +0200

    :fire: Deleted files in gitignore

--- src/domain/Admin.go
+++
@@ -1,7 +0,0 @@
-package domain
-
-type Admin struct {
-	ID string `bson:"_id" json:"id"`
-	Username string `bson:"username" json:"username"`
-	Person Person `bson:"person" json:"person"`
-}
--- src/dto/privacyCheck_dto.go
+++
@@ -1,5 +0,0 @@
-package dto
-
-type PrivacyCheckDto struct {
-	Id string `json:"id"`
-}
